96f1f69ae48bd66f7db67a6b208e719d7d16eaae
JernejKatapult <jernej.hm@gmail.com>
2024-01-12 22:53:53 +0100

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"upload upload" 
+        "config config"
+        "summary basket"`,
+        lg: `"upload upload" 
+        "config summary"
+        "config basket"`,
+      }}
+    >
+      <GridItem area="upload" bg="coral">
+        Upload
+      </GridItem>
+      <GridItem area="config" bg="gold">
+        Config
+      </GridItem>
+      <GridItem area="summary" bg="dodgerblue">
+        Summary
+      </GridItem>
+      <GridItem area="basket" bg="red">
+        Basket
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
